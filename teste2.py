c36fdd75c86a389b6ad1d7ddc0429b50b384797a
Tiago Melo <32605296+trsmelo@users.noreply.github.com>
2017-11-27 01:27:58 -0200

    Commit inicial

---
+++ teste2.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python !',
+        'description': u'Need to find a good Python tutorial on the Web',
+        'done': False
+    }
+]
+
+
+@app.route('/todo/api/v.01/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v.01/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v.01/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not'title' in request.json:
+        abort(404)
+    task ={
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    task.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
